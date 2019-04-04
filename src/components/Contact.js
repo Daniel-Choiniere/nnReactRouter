7c27b3a88ac5e2f264fc84e024641ba097a332ff
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-04-04 02:23:40 +0000

    installed react-router-dom npm package

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const About = () => {
+const Contact = () => {
     return (
         <div className="container">
-            <h4 className="center">About</h4>
+            <h4 className="center">Contact</h4>
             <p>Materialize comes in two different forms. You can select which version you want depending on your preference and expertise. To start using Materialize, all you have to do is download one of the options below.</p>
         </div>
         );
 };
 
-export default About;
+export default Contact;
