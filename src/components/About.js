f76297c980d1f4cc9f20ae26840abb019ef729ac
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-04-04 01:46:16 +0000

    built out the skeleton of the home page, and the about page

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className="container">
+            <h4 className="center">Home</h4>
+            <p>Materialize comes in two different forms. You can select which version you want depending on your preference and expertise. To start using Materialize, all you have to do is download one of the options below.</p>
+        </div>
+        );
+};
+
+export default Home;
