cb0e11ed0605df9ed155c3c82b2c4459ae81ea4c
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-04-04 02:50:51 +0000

    set up the React app to be able to use different routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar';
+import { BrowserRouter, Route} from 'react-router-dom';
+import Home from './components/Home'
+import Contact from './components/Contact'
+import About from './components/About'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-      </div>
+      // sets up the application so it can use different routes
+      //  whenever the user goes to this endpoint (path) than load in this component
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Route path='/' component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/contact' component={Contact} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
