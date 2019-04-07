79acb4fc030120c8203b915880fe659cde786f3e
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-04-07 11:00:10 +0000

    implemented functinality where when user clicks the title of one of the post it brings you to that individual posts profile

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,26 +1,39 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 // we automatically have access to props in class based components
 class Post extends Component {
     state = {
-        id: null
+        post: null
     }
-    
+
+    // grabs the post_id from the props
     componentDidMount() {
-        let id = this.props.match.params.post_id
-        this.setState({
-            id:id
-        });
+        let id = this.props.match.params.post_id;
+        axios.get('https://jsonplaceholder.typicode.com/posts/' + id)
+            .then(res => {
+                this.setState({
+                    post: res.data
+                });
+            });
     }
     render() {
+
+        const post = this.state.post ? (
+            <div>
+                <h4 className="center">{ this.state.post.title }</h4>
+                <p>{ this.state.post.body }</p>
+            </div>
+        ) : (
+            <div className="center">Loading Post</div>
+        )
+
         return (
             <div className="container">
-                <h4>{ this.state.id }</h4>
+                { post }
             </div>
         );
     }
 }
 
 export default Post;
-
-// adding some comment to make a test
